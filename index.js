b2b6585d4926c432442127bce50add6e0aa0daa8
shivshankarsarkate <shivshankarsarkate01@gmail.com>
2024-01-18 18:05:56 +0530

    third

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('hello yeu ka amhi');
+ res.send('hello zoya bhabhi is hot');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
